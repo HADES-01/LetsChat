7b5c3e996bb4f49ede38df4bd15f18498bb8a0ef
HADES-01 <adityavats5338@gmail.com>
2020-09-02 17:58:22 +0530

    Fixed the UI issues

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// DECLARATION OF ALL REQUIRE VARIABLES
+
 let express = require("express"),
     path = require("path"),
     app = express(),
@@ -5,47 +7,71 @@ let express = require("express"),
     io = require("socket.io")(server, {
         perMessageDeflate: false
     }),
+    color = 0,
+    User =  function(username) {  
+        this.username = username;
+        this.color = colors[color++];
+    },  //User model
+    users = [], //List of all the users currently active
     port = process.env.PORT || 3000;
 
+const colors = ['Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque', 'Black', 'BlanchedAlmond', 'Blue', 'BlueViolet', 'Brown', 'BurlyWood', 'CadetBlue', 'Chartreuse', 'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan', 'DarkBlue', 'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGrey', 'DarkGreen', 'DarkKhaki', 'DarkMagenta', 'DarkOliveGreen', 'DarkOrange', 'DarkOrchid', 'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue', 'DarkSlateGray', 'DarkSlateGrey', 'DarkTurquoise', 'DarkViolet', 'DeepPink', 'DeepSkyBlue', 'DimGray', 'DimGrey', 'DodgerBlue', 'FireBrick', 'FloralWhite', 'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod', 'Gray', 'Grey', 'Green', 'GreenYellow', 'HoneyDew', 'HotPink', 'IndianRed ', 'Indigo  ', 'Ivory', 'Khaki', 'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue', 'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGrey', 'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue', 'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 'LightYellow', 'Lime', 'LimeGreen', 'Linen', 'Magenta', 'Maroon', 'MediumAquaMarine', 'MediumBlue', 'MediumOrchid', 'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed', 'MidnightBlue', 'MintCream', 'MistyRose', 'Moccasin', 'NavajoWhite', 'Navy', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed', 'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed', 'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple', 'RebeccaPurple', 'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Salmon', 'SandyBrown', 'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue', 'SlateGray', 'SlateGrey', 'Snow', 'SpringGreen', 'SteelBlue', 'Tan', 'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'Wheat', 'White', 'WhiteSmoke', 'Yellow', 'YellowGreen'
+]; // To Assign a new Color to each new User
+
+// SETTING UP THE SERVER
+
 server.listen(port, () => {
-    console.log("Server is started at http://localhost:%d", port);
+    console.log("[SERVER ONLINE] Server started at http://localhost:%d", port);
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const colors = ['red', 'green', 'blue', 'pink', 'purple'];
 
-let color = 0;
-
-let User =  function(username) {
-    this.username = username;
-    this.color = colors[color++];
-}
-let users = [];
-
-
+// SETTING UP SOCKET TO START LISTENING
 io.on('connect', socket => {
+
+    // Registers each user after checking if they have a unique nickname or not 
+    // and sends a signal to all clients to register the user on their side
     socket.on('hey', (data)=>{
-        let newUser = new User(data)
-        users.push(newUser);
-        socket.emit('user', {userList: users, newUser: newUser});
-        socket.broadcast.emit('joined', newUser);
+        let taken = false;
+        users.forEach((user) => {
+            if(user.username === data){
+                socket.emit('nickname_error');
+                console.log(`[NICKNAME ERROR] ${data} is Taken`);
+                taken = true;
+            }
+        });
+        if(!taken){
+            let newUser = new User(data)
+            console.log(`[NEW CONNECTION] ${newUser.username } is Now Connected.`);
+            console.log(`[USER COUNT] ${color} users online`);
+            users.push(newUser);
+            socket.emit('user', {userList: users, newUser: newUser});
+            socket.broadcast.emit('joined', newUser);
+        }
     });
 
+    // Listens for an incoming message and passes it on to the other users
     socket.on("sendMsg", (data) => {
-        // console.log(data);
         socket.broadcast.emit("recieveMsg", {user: data.user, message: data.message});
     }); 
 
+    // Listens for the 'typing' signal and tell other clients who is typing 
     socket.on('typing', (data) => {
         socket.broadcast.emit("isTyping", data);
     });
 
+    // Listens for the 'notTyping' signal and tell other clients that the user stopped typing 
     socket.on('notTyping', (data) => {
         socket.broadcast.emit("isNotTyping", data);
     });
 
+    // Listens for an incoming request to leave the server and takes it off the users list
+    // and tells other clients tabout the same
     socket.on('leave', (data) => {
+        color--;
+        console.log(`[USER LEFT] ${data.username} is now Offline`)
+        console.log(`[USER COUNT] ${color} users online`);
         socket.broadcast.emit("left", data);
         users = users.filter((user) => {return user.username !== data.username;});
     });
--- public/index.js
+++ public/index.js
@@ -1,31 +1,53 @@
 const socket = io();
+
+//DECLARATION OF ALL VARIABLES REQUIRED
+
 let message_list = document.getElementById("message-list"),
     nickname = document.getElementById('nickname'),
     message = document.getElementById('message').value,
     message_input = document.getElementById('message'),
     username = nickname.value,
-    user_list = document.getElementById('user-list');
+    isLoggedIn = false,
+    user_list = document.getElementById('user-list'),
+    newUser;
 
-let newUser;
 
-const newMessage = (content, color) => {
+//DEFINITION OF EACH FUNCTION USED
+
+// newMesaage() function adds a new 'li' item to the 'ul#message-list' with the respected message
+// that came over the network   
+const newMessage = (data) => {
     const message = document.createElement('li');
-    message.innerHTML = content;
-    message.style.borderLeft = `5px solid ${color}`;
+    message.innerHTML = "<strong class='user-name' style=\'color:" 
+                + data.user.color 
+                + "\'>" 
+                + data.user.username 
+                + "</strong> <br>" 
+                + data.message;
+    message.style.borderLeft = `5px solid ${data.user.color}`;
     message.style.margin = 'auto';
-    message.classList.add("list-group-item", "w-50", "mt-2", "ml-2", "px-2", "py-0");
+    message.classList.add("list-group-item", "w-50", "mb-2", "ml-2", "px-2", "py-0");
     return message;
 }
 
-const sendMessage = (content, color) => {
+// sendMesaage() function adds a new 'li' item to the 'ul#message-list' with the respected message
+// that this user wishes to send
+const sendMessage = (user, message_text) => {
     const message = document.createElement('li');
-    message.innerHTML = content;
-    message.style.borderRight = `5px solid ${color}`;
+    message.innerHTML = "<strong class='user-name' style=\'color:" 
+                + user.color 
+                + "\'>" 
+                + user.username 
+                + "</strong> <br>" 
+                + message_text;
+    message.style.borderRight = `5px solid ${user.color}`;
     message.style.marginRight = "-45%";
-    message.classList.add("list-group-item", "w-50", "mt-2", "ml-2", "px-2", "py-0");
+    message.classList.add("list-group-item", "w-50", "mb-2", "ml-2", "px-2", "py-0");
     return message;
 }
 
+// newUserItem() function adds a new 'li' item to the 'ul#users' with the name of the 
+// new user that just joined  
 const newUserItem = (data) => {
     const user = document.createElement('li');
     user.classList.add("list-group-item");
@@ -35,100 +57,171 @@ const newUserItem = (data) => {
     return user;
 };
 
-const messagePopup = (content, color) => {
+// mesaagePopup() function adds a new 'li' item to the 'ul#message-list' with the information
+// about the joining and leaving of people from the chat   
+const messagePopup = (data, type) => {
     const message = document.createElement('li');
-    message.innerHTML = content;
-    message.classList.add("msg", "w-25", "mt-2", "ml-2", "p-2", "shadow-lg", "bg-white", "rounded");
+    message.innerHTML = "<strong class='user-name' style=\'color:"
+                        + data.color
+                        + "\'>"
+                        + data.username +
+                        "</strong>  " + type;
+    message.classList.add("msg", "w-25", "mb-2", "ml-2", "p-2", "shadow-lg", "bg-white", "rounded","py-0");
     message.style.background = 'rgb(247,243,218)';
-    message.classList.add("py-0");
     return message;
 }
 
+// isTypingMsg() function makes p#isTyping vivsible with the information
+// about the respective user typing a message 
 const isTypingMsg = (user) => {
     const message = document.getElementById('isTyping');
     message.style.display = 'block';
-    message.innerHTML = "<strong class='user-name' style=\'color:" + user.color + "\'>" + user.username + "</strong> <br>" +
-        " <span id='dot'><i class=\'fas fa-circle\'></i> </span><span id='dot'><i class=\'fas fa-circle\'> </i></span><span id='dot'><i class=\'fas fa-circle\'> </i></span>";
+    message.innerHTML = "<strong class='user-name' style=\'color:" 
+                        + user.color 
+                        + "\'>" 
+                        + user.username 
+                        + "</strong> <br>" 
+                        + " <span id='dot'><i class=\'fas fa-circle\'></i> </span>" 
+                        + "<span id='dot'><i class=\'fas fa-circle\'> </i></span> " 
+                        + "<span id='dot'><i class=\'fas fa-circle\'> </i></span>";
     message.style.borderLeft = `5px solid ${user.color}`;
-    message.classList.add("msg", "w-25", "mt-2", "ml-2", "p-2", "shadow-lg", "bg-white", "rounded","py-0");
+    message.classList.add("msg", "list-group-item", "w-25", "mb-2", "ml-2", "p-2", "py-0", "shadow-lg", "rounded");
     message.style.background = 'rgb(247,243,218)';
     return message;
 }
 
+
+// isTypingMsg() function makes p#isTyping invisible that has the information
+// about the respective user typing a message
 const isNotTypingMsg = (user) => {
     const p = document.getElementById('isTyping');
     p.style.display = 'none';
 }
 
-// nickname.onkeydown = nickname_input;
-nickname.onfocus = nickname_input;
 
-function nickname_input(){
-    nickname.onkeydown = (e) => {
-        username = document.getElementById('nickname').value;
-        if (username.length > 0 && e.code === 'Enter'){
-            socket.emit('hey', username);
-            socket.on('nickname_error', () => {
-                document.getElementById('nickname-taken').style.display = 'block';
-                document.getElementById('nickname').focus();
-            });
-            document.getElementById("login-form").style.display = "none";
-            document.getElementById("message-board").style.display = "block";
-        }
+// joinChat() function lets the user join the chat when they have entered their
+// respective nickname 
+function joinChat() {
+    username = document.getElementById('nickname').value;
+    if(username.length > 0) {
+        let taken = false;
+        socket.emit('hey', username);
+        document.getElementById("login-form").style.display = "none";
+        document.getElementById("message-board").style.display = "block";
+        isLoggedIn = true;
     }
 }
 
-message_input.onfocus = (e) => {
-    socket.emit('typing', newUser);
-};
+// nickname_input() function lets the user join the chat when they have entered their
+// respective nickname and pressed enter 
+function nickname_input(e){
+    if (e.code === 'Enter'){
+        joinChat();
+    }
+}
 
-message_input.onblur = (e) => {
-    socket.emit('notTyping', newUser);
-};
-
+// sendMsg() function lets the user send their message on to the sever to be seen by every one
 function sendMsg() {
     message = document.getElementById('message').value;
     if(message.length > 0){
         socket.emit('sendMsg', {user: newUser, message: message})
-        message_list.appendChild(sendMessage("<strong class='user-name' style=\'color:" + newUser.color + "\'>" + newUser.username + "</strong> <br>" + message, newUser.color));
+        message_list.appendChild(sendMessage(newUser, message));
         document.getElementById('message').value = '';
+        document.getElementById('message').focus();
+        document.getElementById('message-list').scrollIntoView(
+            { 
+                behavior: 'smooth', 
+                block: 'end', 
+                inline: 'end' 
+            }
+        );
     }
 }
 
+nickname.onkeydown = nickname_input; //records everytime when a key is pressed on the input and takes reasonable measures
+
+// sends the 'typing' info on the server when a user starts typing on the message input 
+message_input.onkeydown = (e) => {
+    socket.emit('typing', newUser);
+    document.getElementById('message-list').scrollIntoView(
+        { 
+            behavior: 'smooth', 
+            block: 'end', 
+            inline: 'end' 
+        }
+    );
+};
+
+// sends the 'notTyping' info on the server when a user stops typing on the message input
+message_input.onblur = (e) => {
+    socket.emit('notTyping', newUser);
+};
+
+
+// LISTENERS FOR THE EVENTS HAPPENING ON THE SERVER
+
+// tells to stop executing when the server detects a duplicate 'nickname'
+socket.on('nickname_error', () => {
+    document.getElementById("login-form").style.display = "block";
+    document.getElementById("message-board").style.display = "none";
+    document.getElementById('nickname-taken').style.display = 'block';
+    taken = true;
+    isLoggedIn = false;
+});
+
+// recieves the info about the current users on the server
 socket.on('user', (data) => {
     newUser = data.newUser;
-    console.log(data.userList);
     data.userList.forEach((user) => {
         user_list.appendChild(newUserItem(user));
     });
 });
 
+// recieves the info about the new users that join the srever
 socket.on('joined', (data) => {
-    message_list.appendChild(messagePopup("<strong class='user-name' style=\'color:" + data.color + "\'>" + data.username + "</strong> joined", data.color));
+    message_list.appendChild(messagePopup(data, 'joined'));
     user_list.appendChild(newUserItem(data));
+    document.getElementById('message-list').scrollIntoView(
+        { 
+            behavior: 'smooth', 
+            block: 'end', 
+            inline: 'end' 
+        }
+    );
 });
 
-socket.on('recieveMsg', (data) => {
-    let newLi = newMessage("<strong class='user-name' style=\'color:" + data.user.color + "\'>" + data.user.username + "</strong> <br>" + data.message, data.user.color);
-    message_list.appendChild(newLi);
-    message_list.scrollTop = message_list.scrollHeight - newLi.clientHeight;
+// recieves the message send by the users on the server 
+socket.on('recieveMsg', (data) => { 
+    message_list.appendChild(newMessage(data));
+    document.getElementById('message-list').scrollIntoView(
+        { 
+            behavior: 'smooth', 
+            block: 'end', 
+            inline: 'end' 
+        }
+    );
 });
 
+//recieves the info about the user who is typing
 socket.on('isTyping', (data) => {
     isTypingMsg(data);
 });
 
+//recieves the signal when the user stops typing
 socket.on('isNotTyping', (data) => {
     isNotTypingMsg(data);
 });
 
+//recieves the info when a user leaves the server
 socket.on('left', (data) => {
-    message_list.appendChild(messagePopup("<strong class='user-name' style=\'color:" + data.color + "\'>" + data.username + "</strong> left", data.color));
+    message_list.appendChild(messagePopup(data, 'left'));
     document.getElementById(data.username).style.display = 'none';
-
 });
 
+// sends the signal to server to disconnect when the client closes the chat window
 window.addEventListener('beforeunload', () => {
-    socket.emit('leave', newUser);
-    socket.emit('notTyping', newUser);
+    if(isLoggedIn){
+        socket.emit('leave', newUser);
+        socket.emit('notTyping', newUser);
+    }
 });
