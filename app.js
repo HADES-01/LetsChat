9f8d6f2ad18b7460e16a351820ed4bb417d56928
HADES-01 <adityavats5338@gmail.com>
2020-09-18 17:21:30 +0530

    Added About Us page

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 // DECLARATION OF ALL REQUIRE VARIABLES
 let express = require("express"),
-    path = require("path"),
-    app = express(),
-    bodyParser = require("body-parser"),
-    server = require("http").createServer(app),
-    io = require("./io").initialize(server),
-    port = process.env.PORT || 3000,
-    generalRoutes = require("./routes/general");
+  path = require("path"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  server = require("http").createServer(app),
+  io = require("./io").initialize(server),
+  port = process.env.PORT || 3000,
+  generalRoutes = require("./routes/general");
 
 // SETTING UP THE SERVER
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/about", (req, res)=> {
-    res.render("About");
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.use("/chat", generalRoutes);
 
-app.get("/", (req, res)=> {
-	res.render("index");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", () => {});
 
 server.listen(port, () => {
-    console.log("[SERVER ONLINE] Server started at http://localhost:%d", port);
+  console.log("[SERVER ONLINE] Server started at http://localhost:%d", port);
 });
